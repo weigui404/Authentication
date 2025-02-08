b497248278784c9a3315a34e238ae137e5fbcfb7
Jamie Hodgson <jamie-hodgson@live.com>
2025-02-08 14:09:00 +0100

    update preset colours for latest material theme

--- Stratum.Droid/src/Interface/AccentColourMap.cs
+++ Stratum.Droid/src/Interface/AccentColourMap.cs
@@ -32,22 +32,22 @@ namespace Stratum.Droid.Interface
         {
             return name switch
             {
-                "red" => Resource.Color.md_theme_light_red_primary,
-                "pink" => Resource.Color.md_theme_light_pink_primary,
-                "purple" => Resource.Color.md_theme_light_purple_primary,
-                "deepPurple" => Resource.Color.md_theme_light_deepPurple_primary,
-                "indigo" => Resource.Color.md_theme_light_indigo_primary,
-                "blue" => Resource.Color.md_theme_light_blue_primary,
-                "lightBlue" => Resource.Color.md_theme_light_lightBlue_primary,
-                "cyan" => Resource.Color.md_theme_light_cyan_primary,
-                "teal" => Resource.Color.md_theme_light_teal_primary,
+                "red" => Resource.Color.md_theme_red_primary,
+                "pink" => Resource.Color.md_theme_pink_primary,
+                "purple" => Resource.Color.md_theme_purple_primary,
+                "deepPurple" => Resource.Color.md_theme_deep_purple_primary,
+                "indigo" => Resource.Color.md_theme_indigo_primary,
+                "blue" => Resource.Color.md_theme_blue_primary,
+                "lightBlue" => Resource.Color.md_theme_light_blue_primary,
+                "cyan" => Resource.Color.md_theme_cyan_primary,
+                "teal" => Resource.Color.md_theme_teal_primary,
                 "green" => Resource.Color.md_theme_light_green_primary,
-                "lightGreen" => Resource.Color.md_theme_dark_lightGreen_primary,
-                "lime" => Resource.Color.md_theme_light_lime_primary,
-                "yellow" => Resource.Color.md_theme_light_yellow_primary,
-                "amber" => Resource.Color.md_theme_light_amber_primary,
-                "orange" => Resource.Color.md_theme_light_orange_primary,
-                "deepOrange" => Resource.Color.md_theme_light_deepOrange_primary,
+                "lightGreen" => Resource.Color.md_theme_light_green_primary,
+                "lime" => Resource.Color.md_theme_lime_primary,
+                "yellow" => Resource.Color.md_theme_yellow_primary,
+                "amber" => Resource.Color.md_theme_amber_primary,
+                "orange" => Resource.Color.md_theme_orange_primary,
+                "deepOrange" => Resource.Color.md_theme_deep_orange_primary,
                 _ => throw new ArgumentOutOfRangeException(nameof(name))
             };
         }
